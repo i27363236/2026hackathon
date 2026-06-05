3ec2e06a674d5a0f4548fae24743cac87675f9bd
wwyi0 <kuoeling@gmail.com>
2026-06-05 20:21:31 +0800

    Portfolio的程式碼

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Portfolio from './components/Portfolio.vue';
 </script>
 
 <template>
-  <HelloWorld />
+  <!-- <HelloWorld /> -->
+  <Portfolio />
 </template>
