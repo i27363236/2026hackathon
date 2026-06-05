a1b3402a63cb553fc42ba9840b9e8bfc502f3269
shermyliou <shermy.liou@gmail.com>
2026-06-05 19:46:59 +0800

    新增優惠券和活動資料，擴充應用程式的功能性

--- src/data/coupons.js
+++ src/data/coupons.js
@@ -3,10 +3,18 @@
 // Each item carries a stable id and a colorKey (variant-1 … variant-6, see _card-colors.scss).
 
 const coupons = [
-  { id: 'cpn-1', title: '台北捷運', point: 50, sub: '50元電子乘車金', colorKey: 'variant-2' },
-  { id: 'cpn-2', title: '東森購物', point: 50, sub: '50元折價券', colorKey: 'variant-4' },
-  { id: 'cpn-3', title: '全家便利商店', point: 50, sub: '50元折價券', colorKey: 'variant-1' },
-  { id: 'cpn-4', title: '誠品生活', point: 50, sub: '50元抵用券', colorKey: 'variant-5' },
+  { id: 'cpn-1',  title: '台北捷運',    point: 50,  sub: '50元電子乘車金',  colorKey: 'variant-2' },
+  { id: 'cpn-2',  title: '東森購物',    point: 50,  sub: '50元折價券',      colorKey: 'variant-4' },
+  { id: 'cpn-3',  title: '全家便利商店', point: 50,  sub: '50元折價券',      colorKey: 'variant-1' },
+  { id: 'cpn-4',  title: '誠品生活',    point: 50,  sub: '50元抵用券',      colorKey: 'variant-5' },
+  { id: 'cpn-5',  title: '7-ELEVEN',   point: 100, sub: '100元折價券',     colorKey: 'variant-3' },
+  { id: 'cpn-6',  title: '星巴克',      point: 150, sub: '中杯飲品兌換券',   colorKey: 'variant-6' },
+  { id: 'cpn-7',  title: '麥當勞',      point: 80,  sub: '麥香魚套餐兌換券', colorKey: 'variant-2' },
+  { id: 'cpn-8',  title: 'IKEA',       point: 200, sub: '200元購物金',     colorKey: 'variant-4' },
+  { id: 'cpn-9',  title: '博客來',      point: 100, sub: '100元折價券',     colorKey: 'variant-1' },
+  { id: 'cpn-10', title: 'momo購物',    point: 120, sub: '120元優惠券',     colorKey: 'variant-5' },
+  { id: 'cpn-11', title: '路易莎咖啡',   point: 60,  sub: '中杯咖啡兌換券',   colorKey: 'variant-3' },
+  { id: 'cpn-12', title: '王品集團',     point: 300, sub: '餐廳折抵券',      colorKey: 'variant-6' },
 ]
 
 export function getCoupons() {
--- src/data/events.js
+++ src/data/events.js
@@ -6,6 +6,14 @@ const events = [
   { id: 'evt-2', title: '信義計畫區音樂會', tag: '台北捷運', img: '#cfe1e8', colorKey: 'variant-4' },
   { id: 'evt-3', title: '淡水老街美食探索', tag: '淡水', img: '#e8dfc8', colorKey: 'variant-3' },
   { id: 'evt-4', title: '士林夜市市集', tag: '士林', img: '#e8cfd8', colorKey: 'variant-5' },
+  { id: 'evt-5', title: '台北車站街頭音樂節', tag: '台北車站', img: '#cfe5f0', colorKey: 'variant-2' },
+  { id: 'evt-6', title: '信義區藝術展', tag: '信義', img: '#cfe2d0', colorKey: 'variant-6' },
+  { id: 'evt-7', title: '大稻埕文創市集', tag: '大稻埕', img: '#e8c0b8', colorKey: 'variant-3' },
+  { id: 'evt-8', title: '北投溫泉祭', tag: '北投', img: '#cfe0e8', colorKey: 'variant-4' },
+  { id: 'evt-9', title: '松山夜跑', tag: '松山', img: '#e8e0c8', colorKey: 'variant-1' },
+  { id: 'evt-10', title: '南港科技講座', tag: '南港', img: '#cfe8d0', colorKey: 'variant-5' },
+  { id: 'evt-11', title: '文山書展', tag: '文山', img: '#e8d8f8', colorKey: 'variant-2' },
+  { id: 'evt-12', title: '內湖運動會', tag: '內湖', img: '#cfe4c8', colorKey: 'variant-6' },
 ]
 
 export function getEvents() {
